f39550019f195abce77293675aaccb9adf45f1e7
Nick Pfister <me@nickpfister.com>
2019-10-24 15:58:19 -0700

    Started ch 9

---
+++ Chapter 9/ch9e1.js
@@ -0,0 +1,45 @@
+// Fill in the regular expressions
+
+verify(/ca(t|r)/, ["my car", "bad cats"], ["camper", "high art"]);
+
+verify(/pr?op/, ["pop culture", "mad props"], ["plop", "prrrop"]);
+
+verify(
+  /ferr(et|y|ari)/,
+  ["ferret", "ferry", "ferrari"],
+  ["ferrum", "transfer A"]
+);
+
+verify(/ious\b/, ["how delicious", "spacious room"], ["ruinous", "consciousness"]);
+
+verify(/\s[.,:;]/, ["bad punctuation ."], ["escape the period"]);
+
+verify(/\w{1-6}/, ["hottentottententen"], ["no", "hotten totten tenten"]);
+
+verify(
+  /.../,
+  ["red platypus", "wobbling nest"],
+  ["earth bed", "learning ape", "BEET"]
+);
+
+function verify(regexp, yes, no) {
+  // Ignore unfinished exercises
+  let failed = false;
+  if (regexp.source == "...") return;
+  for (let str of yes)
+    if (!regexp.test(str)) {
+      console.log(`Failure to match '${str}'`);
+      failed = true;
+    }
+  for (let str of no)
+    if (regexp.test(str)) {
+      console.log(`Unexpected match for '${str}'`);
+      failed = true;
+    }
+  let test = `Yes: ${yes.join(", ")} No: ${no.join(", ")}`;
+  if (!failed) {
+    console.log("Test passed! " + test);
+  } else {
+    console.log("Test failed! " + test);
+  }
+}
